7011bfc02741f384a36497cda7de9ffdf66cb7ec
Katarzyna Zukowska <katarzynazukowska@KatarzynaZukowska-MBP.local>
2019-03-22 13:15:09 +0100

    Darken on hover

---
+++ src/Experience/Experience.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './experience.scss';
+
+class Experience extends React.Component {
+
+  constructor(props) {
+
+  }
+
+
+  render() {
+
+    return(
+      <div className="experience">
+
+      </div>
+    )
+  }
+}
+
+export default Project;
