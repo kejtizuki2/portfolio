189e07cba7df962afe3de9436cd1418d8bfed747
Kasia Zukowska <kzukowska@hotmail.com>
2019-05-03 08:22:41 +0200

    Updates

---
+++ precache-manifest.b21597fac7ae754da2c10e42730f022f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c973ea5a7e79ae024fb7837b397a2ccb",
+    "url": "/portfolio/static/media/main.c973ea5a.png"
+  },
+  {
+    "revision": "11836464f228c1f4b955",
+    "url": "/portfolio/static/css/main.77831e2a.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "9ce412891e42f2a1ff03",
+    "url": "/portfolio/static/js/2.29f3edbf.chunk.js"
+  },
+  {
+    "revision": "45a81af52536d9b9e2357c67ff86af4a",
+    "url": "/portfolio/static/media/sunwise-main1.45a81af5.jpg"
+  },
+  {
+    "revision": "0432f01c4404161864fcd06c1ae57b11",
+    "url": "/portfolio/static/media/dogo.0432f01c.png"
+  },
+  {
+    "revision": "087093de122d291f644bf6a025453bb0",
+    "url": "/portfolio/static/media/main.087093de.png"
+  },
+  {
+    "revision": "8f026041adea98602579a9df4fd190e8",
+    "url": "/portfolio/static/media/main3.8f026041.png"
+  },
+  {
+    "revision": "11836464f228c1f4b955",
+    "url": "/portfolio/static/js/main.c93d0ec8.chunk.js"
+  },
+  {
+    "revision": "426c5f2ca1fb7aa50308858e9b0580e1",
+    "url": "/portfolio/static/media/screen_home.426c5f2c.png"
+  },
+  {
+    "revision": "819ca87419aca945f66899ba096809de",
+    "url": "/portfolio/static/media/sunwise-main.819ca874.png"
+  },
+  {
+    "revision": "e5cb6776ad7d7f2634a0f946a461ca38",
+    "url": "/portfolio/static/media/firstPrototype.e5cb6776.png"
+  },
+  {
+    "revision": "08b5c9232df56a5daf839ff8bb9746d3",
+    "url": "/portfolio/static/media/old1-fix.08b5c923.png"
+  },
+  {
+    "revision": "218c84cbd4dd064755241a6c57238d06",
+    "url": "/portfolio/static/media/old2-fix.218c84cb.png"
+  },
+  {
+    "revision": "8e990a2c53afec13c5b1ecebf029b944",
+    "url": "/portfolio/static/media/old3-fix.8e990a2c.png"
+  },
+  {
+    "revision": "188495f7cd9738435a6868818e8621f8",
+    "url": "/portfolio/static/media/old4-fix.188495f7.png"
+  },
+  {
+    "revision": "e3914254b6335d63c77c81842c0cda03",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d1896ae248029cf7a1138b6495e659c4.js"
+  "/portfolio/precache-manifest.b21597fac7ae754da2c10e42730f022f.js"
 );
 
 workbox.clientsClaim();
