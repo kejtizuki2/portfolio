c9db6be4487f9b52e9767bdbce5d0249f599704e
Kasia Zukowska <kzukowska@hotmail.com>
2019-05-09 23:56:45 +0200

    Updates

---
+++ precache-manifest.a229f1bd635204989e6cdcf0dc6f74e5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "9182524f6a3e5539500f2dce4c94c0d4",
+    "url": "/portfolio/static/media/feedback.9182524f.png"
+  },
+  {
+    "revision": "15e48c4948c21f78cee0",
+    "url": "/portfolio/static/css/main.83f95d22.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "82a3abd193e0d9510fbc",
+    "url": "/portfolio/static/js/2.f43cdbfc.chunk.js"
+  },
+  {
+    "revision": "45a81af52536d9b9e2357c67ff86af4a",
+    "url": "/portfolio/static/media/sunwise-main1.45a81af5.jpg"
+  },
+  {
+    "revision": "0432f01c4404161864fcd06c1ae57b11",
+    "url": "/portfolio/static/media/dogo.0432f01c.png"
+  },
+  {
+    "revision": "087093de122d291f644bf6a025453bb0",
+    "url": "/portfolio/static/media/main.087093de.png"
+  },
+  {
+    "revision": "8f026041adea98602579a9df4fd190e8",
+    "url": "/portfolio/static/media/main3.8f026041.png"
+  },
+  {
+    "revision": "c973ea5a7e79ae024fb7837b397a2ccb",
+    "url": "/portfolio/static/media/main.c973ea5a.png"
+  },
+  {
+    "revision": "59840adc7383b0a7e80d1a60d5185e54",
+    "url": "/portfolio/static/media/cv-katarzyna-zukowska.59840adc.pdf"
+  },
+  {
+    "revision": "819ca87419aca945f66899ba096809de",
+    "url": "/portfolio/static/media/sunwise-main.819ca874.png"
+  },
+  {
+    "revision": "e5cb6776ad7d7f2634a0f946a461ca38",
+    "url": "/portfolio/static/media/firstPrototype.e5cb6776.png"
+  },
+  {
+    "revision": "15e48c4948c21f78cee0",
+    "url": "/portfolio/static/js/main.4455f3ea.chunk.js"
+  },
+  {
+    "revision": "426c5f2ca1fb7aa50308858e9b0580e1",
+    "url": "/portfolio/static/media/screen_home.426c5f2c.png"
+  },
+  {
+    "revision": "8d612bf1e29270614f8654871e82eba1",
+    "url": "/portfolio/static/media/statistics.8d612bf1.gif"
+  },
+  {
+    "revision": "00997acf71cfac724800e99f9997754d",
+    "url": "/portfolio/static/media/screen_calendar.00997acf.png"
+  },
+  {
+    "revision": "c5c3d44f8ce5812f12902117396da23e",
+    "url": "/portfolio/static/media/screen_about.c5c3d44f.png"
+  },
+  {
+    "revision": "8c0fa561c595373452233ee63d158075",
+    "url": "/portfolio/static/media/presentX.8c0fa561.png"
+  },
+  {
+    "revision": "6b79e702653e3a445ec7872d957b7c76",
+    "url": "/portfolio/static/media/screens.6b79e702.png"
+  },
+  {
+    "revision": "4213ce21c650eebcc8765b799f9838b7",
+    "url": "/portfolio/static/media/main-view.4213ce21.png"
+  },
+  {
+    "revision": "dfbcb460d9840db6380390ccd37106f2",
+    "url": "/portfolio/static/media/map.dfbcb460.gif"
+  },
+  {
+    "revision": "162c71da761781641564fbd90eaa79ea",
+    "url": "/portfolio/static/media/distributions.162c71da.gif"
+  },
+  {
+    "revision": "e0e9514d863d90d19b21c751b26def28",
+    "url": "/portfolio/static/media/Advanced_Project.e0e9514d.pdf"
+  },
+  {
+    "revision": "5ed2f6039b38fdf8a82d10f10dd2e6d9",
+    "url": "/portfolio/static/media/main.5ed2f603.png"
+  },
+  {
+    "revision": "6a529afe0ac310122d5311f0542a7931",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.089daf09b17453fb8714b3692942e4c2.js"
+  "/portfolio/precache-manifest.a229f1bd635204989e6cdcf0dc6f74e5.js"
 );
 
 workbox.clientsClaim();
