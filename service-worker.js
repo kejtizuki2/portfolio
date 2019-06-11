d7d84dd84d9aedcfb0230ee8b07388b4c6c585e8
Kasia Zukowska <kzukowska@hotmail.com>
2019-06-11 23:34:19 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c7df63ee1619312a3a4e39985b423255.js"
+  "/portfolio/precache-manifest.c0b1d2db667ad43c85402285771530c2.js"
 );
 
 workbox.clientsClaim();
