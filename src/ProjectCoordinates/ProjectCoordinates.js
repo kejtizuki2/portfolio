6b4d3a777342bc53b1ed35d4400cdc9c6fc7f1c3
Kasia Zukowska <kzukowska@hotmail.com>
2019-06-11 23:33:30 +0200

    coordinates update

--- src/ProjectCoordinates/ProjectCoordinates.js
+++ src/ProjectCoordinates/ProjectCoordinates.js
@@ -40,9 +40,59 @@ interesting stories about the data.
         <div className="col3"></div>
         </div>
 
-        <img src={require("./../assets/img/Coordinates/dimensions.gif")} className="mainImg"/>
-        <img src={require("./../assets/img/Coordinates/brush-simple.gif")} className="mainImg"/>
-        <img src={require("./../assets/img/Coordinates/line-brush.gif")} className="mainImg"/>
+        <div className="row">
+        <div className="col7"><h2 className="title">Dimensions</h2><p>The default behavior when uploading
+          a .csv dataset is to render the
+          parallel coordinates based on all dimensions, sorted in alphabetical
+          order. However, one can remove (or add) dimensions in
+          case they are not needed by toggling the corresponding dimension
+          button and also change the order of the vertical axes by dragging
+          them to left or right.
+        </p></div>
+        <div className="col3"></div>
+        </div>
+
+          <img src={require("./../assets/img/Coordinates/dimensions.gif")} className="mainImg"/>
+
+        <div className="row">
+        <div className="col7"><h2 className="title">Direct Selection</h2><p>Direct selection, also known as 0-order
+brushing, occurs when you hover over one line in the chart. More
+specifically, each time the user places the mouse over a polyline,
+additional information about its corresponding dataset entry will
+be displayed in a table below.
+If user clicks on a highlighted line, that line remains saved in the
+table for further exploration until removed. Users are allowed to
+select multiple lines from the chart and compare their values in
+the table. The "Reset" button deselects all the
+lines and the plot goes back to its initial state.
+        </p></div>
+        <div className="col3"></div>
+        </div>
+
+        <img src={require("./../assets/img/Coordinates/direct-selection.png")} className="mainImg"/>
+
+        <div className="row">
+        <div className="col7"><h2 className="title">Simple Brushing</h2><p>Simple brushing is the most basic form
+of brushing. The central aim of this technique is to
+restrict the dimensions between a min and max value.
+        </p></div>
+        <div className="col3"></div>
+        </div>
+
+        <img src={require("./../assets/img/Coordinates/simple-brush.png")} className="mainImg"/>
+
+        <div className="row">
+        <div className="col7"><h2 className="title">Line Brushing</h2><p>Line brushing is a 2D selection brushing
+method which can be performed by drawing lines in the areas
+between each 2 axes. The
+selection contains all entries that pass through all of the active
+lines (Figure 8). The users can remove the lines and draw new ones
+by clicking anywhere close to their surrounding area.
+        </p></div>
+        <div className="col3"></div>
+        </div>
+
+        <img src={require("./../assets/img/Coordinates/line-brush.png")} className="mainImg"/>
       </div>
     )
   }
