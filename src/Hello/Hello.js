c379401823f72afa3815d07052d5c600d18cdefa
Kasia Zukowska <kzukowska@hotmail.com>
2019-07-09 09:51:24 +0200

    photo

--- src/Hello/Hello.js
+++ src/Hello/Hello.js
@@ -15,6 +15,7 @@ export default class Hello extends React.Component {
           <a href="https://www.linkedin.com/in/katarzyna-%C5%BCukowska-23398194/"><img src={require("./../assets/img/001-linkedin.png")} alt="linkedin"/></a>
           <a href="https://www.behance.net/kejtizukife8c"><img src={require("./../assets/img/002-behance.png")} alt="https:/google.com"/></a>
           <a href="https://github.com/kejtizuki"><img src={require("./../assets/img/github.png")} alt="github"/></a>
+          <a href="https://kejtizuki.cargo.site"><img src={require("./../assets/img/photo.png")} alt="photography portfolio"/></a>
         </div>
         </div>
       </div>
